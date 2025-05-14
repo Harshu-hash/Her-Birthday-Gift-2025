10080b99f480bc8f96564474fdac7ec297fb7b50
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-05-14 16:26:52 +0530

    Fix lottie file path and move to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Home.css'
 // import anim from './Animation - 1747210967603.lottie'
-import illus from '/air balloon with clouds.png'
+import illus from '/images/air balloon with clouds.png'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const Home = () => {
   return (
   <>
     <DotLottieReact
-      src='/Animation-1747210967603.lottie'
+      src='/animations/Animation-1747210967603.lottie'
       loop
       autoplay
     />
