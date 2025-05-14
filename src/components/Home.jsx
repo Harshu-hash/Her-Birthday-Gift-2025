abfbeed6949730316d21adec4d4f352100e28075
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-05-14 19:13:22 +0530

    Fix lottie file path and move to public folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import us from '/images/1743332704262.webp.png'
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Home = () => {
@@ -10,6 +11,10 @@ const Home = () => {
     
   <p className="banner-heading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nam recusandae, quisquam quidem reprehenderit, ullam id totam!</p>
   <DotLottieReact className="Happy-birthday-animation" src="\animations\HappyBirthday.lottie" loop autoplay />
+  </div>
+
+  <div className="our-photo">
+    <img src={us} alt="" className="us" />
   </div>
   </div>
   </>;
