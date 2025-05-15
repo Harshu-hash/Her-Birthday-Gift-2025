6016e2132a46522cefed04762ad16c360dc02c09
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-05-15 11:24:42 +0530

    Fix lottie file path and move to public folder

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import './Timeline.css'
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 
 const Timeline = () => {
   return (
-    <div>
-      timeline loading...
-    </div>
-  )
+    <>
+      <div className="timeline-page">
+        <div className="timeline-heading">
+          <h3>Our Timeline</h3>
+        </div>
+        <DotLottieReact
+          className="Happy-birthday-animation"
+          src="\animations\Eyes.lottie"
+          loop
+          autoplay
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          explicabo fugit commodi quidem voluptas quia cupiditate suscipit
+          eveniet quas et, possimus facilis aspernatur asperiores amet
+          dignissimos ipsa, minima voluptatem doloribus ex blanditiis in quasi
+          quisquam? Eum illo reiciendis adipisci, error aspernatur corporis
+          dolorem nihil exercitationem temporibus iusto dignissimos mollitia
+          quam accusantium ex dolore! Sapiente rem autem, ab doloribus iure
+          fugit?{" "}
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Timeline
