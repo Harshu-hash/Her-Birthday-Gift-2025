f3677bc58acd9a19e54316d3d751c601441e61c0
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-05-15 16:25:20 +0530

    Fix lottie file path and move to public folder

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -67,7 +67,7 @@ const Timeline = () => {
           data-aos-offset="200">
           <DotLottieReact
             className="Happy-birthday-animation"
-            src="\animations\BikeRide.lottie"
+            src="\animations\Bikeride.lottie"
             loop
             autoplay
           />
