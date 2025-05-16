bd71b36c4e74f5deab26e1c769e07d7c5d22805b
harshu-panchal <harshvardhanpanc145@gmail.com>
2025-05-16 16:48:04 +0530

    Fix lottie file path and move to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-import Timeline from './components/Timeline';
+import welcome from "/images/welcome.png";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Timeline from "./components/Timeline";
 import { Analytics } from "@vercel/analytics/react";
 
-import { useEffect } from "react";
-import React from "react";
+import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 function App() {
+  const [unlocked, setUnlocked] = useState(false);
+  const [code, setCode] = useState("");
 
   useEffect(() => {
     AOS.init({
@@ -47,13 +50,52 @@ function App() {
       ),
     },
   ]);
+  // Check input code
+  const handleUnlock = (e) => {
+    e.preventDefault();
+    if (code === "1234") {
+      setUnlocked(true);
+    } else {
+      alert("Wrong code!");
+    }
+  };
 
+  // Show code entry screen until unlocked
+  if (!unlocked) {
+    return (
+      <div className="login-page">
+        <div className="create-animation">
+          <img src={welcome} alt="" className="wlcm-cntanr welcome-img" />
+          {/* <DotLottieReact
+            className="wlcm-cntanr welcome-animation"
+            src="\animations\Celebration.lottie"
+            loop
+            autoplay
+          /> */}
+        </div>
+        <form onSubmit={handleUnlock} className="login-form">
+          <input
+            type="password"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            className="code-input"
+            placeholder="   Enter Secret Code"
+          />
+          <button type="submit" className="login-button">
+            Enter
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  // If unlocked, show actual app
   return (
     <>
       <Analytics />
       <RouterProvider router={router} />
     </>
   );
-};
+}
 
-export default App
+export default App;
